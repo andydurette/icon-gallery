28767e1466a66f2926a003851f156772bd9d1ff2
Andy Durette <duretteandy@gmail.com>
2019-09-05 11:59:56 -0400

    All edits from base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
 import React from 'react';
-import logo from './logo.svg';
+import Lightbox from 'react-lightbox-component';
+import "react-lightbox-component/build/css/index.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <header>
+      <h1>30 Day Icon Challenge</h1>
+      <p>A personal project where I designed an icon daily for a month</p>
+    </header>
+    <Lightbox images={ [
+      {
+        src: 'images/xxxlarge/camera.svg',
+        title: 'Camera',
+      },
+      {
+        src: 'images/xxxlarge/eggs-benedict.jpg',
+        title: 'Eggs Benedict',
+      },
+      {
+        src: 'images/xxxlarge/glasses.jpg',
+        title: 'Full Rim Glasses',
+      },
+      {
+        src: 'images/xxxlarge/bluejay.jpg',
+        title: 'Blue Jay',
+      },
+      {
+        src: 'images/xxxlarge/business-bag.jpg',
+        title: 'Business Bag',
+      },
+      {
+        src: 'images/xxxlarge/violin.jpg',
+        title: 'Violin',
+      },
+      {
+        src: 'images/xxxlarge/nigiri.jpg',
+        title: 'Nigiri',
+      },
+      {
+        src: 'images/xxxlarge/milk.jpg',
+        title: 'Milk',
+      },
+      {
+        src: 'images/xxxlarge/television.jpg',
+        title: 'Television',
+      },
+      {
+        src: 'images/xxxlarge/pancakes.svg',
+        title: 'Pancakes',
+      },
+      {
+        src: 'images/xxxlarge/california-roll.jpg',
+        title: 'California Roll',
+      },
+      {
+        src: 'images/xxxlarge/softserve.svg',
+        title: 'Soft Serve',
+      },
+      {
+        src: 'images/xxxlarge/blender.jpg',
+        title: 'Blender',
+      },
+      {
+        src: 'images/xxxlarge/tardis.svg',
+        title: 'Tardis',
+      },
+      {
+        src: 'images/xxxlarge/ps4-controller.jpg',
+        title: 'PS4 Controller',
+      },
+      {
+        src: 'images/xxxlarge/liberty-bell.jpg',
+        title: 'Liberty Bell',
+      },
+      {
+        src: 'images/xxxlarge/satellite.jpg',
+        title: 'Satellite',
+      },
+      {
+        src: 'images/xxxlarge/chess-queen.jpg',
+        title: 'Chess Queens',
+      },
+      {
+        src: 'images/xxxlarge/cast-iron-pan.svg',
+        title: 'Cast Iron Pan',
+      },
+      {
+        src: 'images/xxxlarge/cell-phone.jpg',
+        title: 'Cell Phone',
+      },
+      {
+        src: 'images/xxxlarge/mixer.svg',
+        title: 'Mixer',
+      },
+      {
+        src: 'images/xxxlarge/headphones.svg',
+        title: 'Headphone',
+      },
+      {
+        src: 'images/xxxlarge/lightbulb.svg',
+        title: 'Lightbulb',
+      },
+      {
+        src: 'images/xxxlarge/taco.svg',
+        title: 'Taco',
+      },
+      {
+        src: 'images/xxxlarge/pinwheels.svg',
+        title: 'Pin Wheels',
+      },
+      {
+        src: 'images/xxxlarge/moon-and-stars.svg',
+        title: 'Pin Wheels',
+      },
+      {
+        src: 'images/xxxlarge/tie-fighter.svg',
+        title: 'Tie Fighter',
+      },
+      {
+        src: 'images/xxxlarge/bowler-hat.svg',
+        title: 'Bowler Hat',
+      },
+      {
+        src: 'images/xxxlarge/ramen.svg',
+        title: 'Ramen',
+      },
+      {
+        src: 'images/xxxlarge/raven.svg',
+        title: 'Raven',
+      },
+      
+    ] }/>
+  </div>
+);
 
 export default App;
